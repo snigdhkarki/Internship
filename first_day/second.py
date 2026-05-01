8fbb26769ffa97f345119833e7fff421bd376ce7
snigdh <snigdhkarki@gmail.com>
2026-05-01 09:45:27 +0545

    final

--- first_day/second.py
+++
@@ -1,8 +0,0 @@
-try:
-    num = int(input("Enter a number to see its multiplication table: "))
-
-    print(f"\nMultiplication Table for {num}:")
-    for i in range(1, 11):
-        print(f"{num} x {i} = {num * i}")
-except ValueError:
-    print("Please enter a valid integer.")
